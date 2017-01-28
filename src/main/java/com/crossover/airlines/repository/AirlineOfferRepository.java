8f93dbf913035d7529bdff2680617671838629bc
sjain <sapan.jain@charter.com>
2017-01-28 02:47:15 -0600

    adding repository for offers

---
+++ src/main/java/com/crossover/airlines/repository/AirlineOfferRepository.java
@@ -0,0 +1,10 @@
+package com.crossover.airlines.repository;
+
+import com.crossover.airlines.domain.AirlineOffer;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by sjain on 1/28/2017.
+ */
+public interface AirlineOfferRepository extends CrudRepository<AirlineOffer,Long>{
+}
