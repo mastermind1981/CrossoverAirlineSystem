bae7389c526e983865ada63a71271566c281c410
smita <smita.s.kothari@gmail.com>
2017-01-28 03:23:04 -0600

    created endpoint and services

---
+++ src/main/java/com/crossover/airlines/repository/AirlineOfferRepository.java
@@ -0,0 +1,10 @@
+package com.crossover.airlines.repository;
+
+import com.crossover.airlines.domain.AirlineOffer;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by smita on 1/28/17.
+ */
+public interface AirlineOfferRepository extends CrudRepository<AirlineOffer,Long>{
+}
